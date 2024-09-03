78db56ab14a34ec97f960db31d9fde6520e6ee36
khairul-islam98 <janeyrahman900@gmail.com>
2024-09-04 03:44:11 +0600

    login register design complete

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
+    const handleSignIn =  () => {
+        const data = {email, password};
+        console.log(data);
+    }
+    
 
   return (
     <div className="h-[100vh] flex items-center justify-center p-10">
@@ -12,10 +20,22 @@ const Login = () => {
           Login{" "}
         </h1>
         <div className="flex flex-col  gap-3 bg-white p-6 min-h-[150px] rounded-xl drop-shadow-xl ">
-          <Input name="email" type="email" />
-          <Input name="password" type="password" />
-          <Button text="Login" />
-          <Link to='/register'><Button className="w-full" text="Register" secondary /></Link>
+          <Input
+            name="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button onclick={handleSignIn} text="Login" />
+          <Link to="/register">
+            <Button className="w-full" text="Register" secondary />
+          </Link>
         </div>
       </div>
       <div className="h-full w-full bg-gradient-to-r opacity-70 from-myBlue to-myPink absolute  top-0 -z-10" />
--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,9 +1,21 @@
 import { Link } from "react-router-dom";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
+import { useState } from "react";
 
 
 const Register = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleSignUp =  () => {
+        const data = {email, password, confirmPassword};
+        console.log(data);
+    }
+    
+
     return (
         <div className="h-[100vh] flex items-center justify-center p-10">
         <div className="w-full md:w-[450px]">
@@ -11,10 +23,10 @@ const Register = () => {
             Register{" "}
           </h1>
           <div className="flex flex-col  gap-3 bg-white p-6 min-h-[150px] rounded-xl drop-shadow-xl ">
-            <Input name="email" type="email" />
-            <Input name="password" type="password" />
-            <Input name="confirm-password" type="password" />
-            <Button text="Register" secondary />
+            <Input name="email" type="email" value={email} onChange={e => setEmail(e.target.value)} />
+            <Input name="password" type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            <Input name="confirm-password" type="password" value={confirmPassword} onChange={e=> setConfirmPassword(e.target.value)}  />
+            <Button onclick={handleSignUp} text="Register" secondary />
             <Link to='/login'><Button className="w-full" text="Login" /></Link>
           </div>
         </div>
